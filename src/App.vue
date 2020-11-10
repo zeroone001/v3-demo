d03082bbb698193db9d041e634b45e79ef4c113a
zeroone001 <ilylys@163.com>
2020-11-10 20:32:37 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,17 @@
 </template>
 
 <script>
+// toRaw 把reactive或者ref类型的数据获取原始数据
+/*
+  ref/reactive 数据类型的特点：响应式数据
+  每次修改都会被追踪，都会更新UI界面，但是这样非常消耗性能，
+  如果一些操作不需要追踪，不需要修改界面，我们就可以通过toRaw 获取原始数据，
+  对数据进行修改，这样性能就好啦
+
+*/
 import HelloWorld from './components/HelloWorld.vue'
 import { removeFun } from './componsition/removeFun.js';
-import { ref, reactive, isRef, isReactive } from 'vue';
+import { toRaw, ref, reactive, isRef, isReactive, shallowReactive, shallowRef, triggerRef } from 'vue';
 export default {
   name: 'App',
   components: {
@@ -25,13 +33,19 @@ export default {
   },
   setup () {
     let msg = ref('我是谁!!!!');
+    let state2 = reactive({
+      name: 'lys',
+      sex: 'qwe'
+    });
+    console.log('toRaw', toRaw(state2));
     console.log('msg-ref', msg);
     let { state, reMov } = removeFun();
-    console.log('reactive', state);
+    console.log('reactive:::', state.stu);
     return {
       msg,
       state,
-      reMov
+      reMov,
+      state2
     }
   }
 };
--- src/componsition/removeFun.js
+++ src/componsition/removeFun.js
@@ -1,17 +1,22 @@
+/**
+ * Description: 请输入文件描述
+ * Author: liuyongsheng
+ * Date: 2020-11-09 15:08:48
+*/
 import { ref, reactive } from 'vue';
 
 export function removeFun () {
     let state = reactive({
         stu: [
-        {
-          name: 'zhangsan',
-          id: '11'
-        },
-        {
-          name: 'lisi',
-          id: '22'
-        }
-      ]
+          {
+            name: 'zhangsan',
+            id: '11'
+          },
+          {
+            name: 'lisi',
+            id: '22'
+          }
+        ]
       });
       function reMov (index) {
         state.stu = state.stu.filter((item, i) => {
